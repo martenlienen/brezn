72505060e22b5ac06306b93479a294b3eb778c4e
Marten Lienen <marten.lienen@gmail.com>
2023-02-14 17:14:55 +0100

    Configure logging

--- brezn/main.py
+++ brezn/main.py
@@ -1,7 +1,18 @@
+import logging
 import click
+
+# Since this module controls the application, we get the root logger here instead of the
+# one specified by __name__
+log = logging.getLogger("brezn")
 
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
-def main():
+@click.option("-v", "--verbose", count=True)
+def main(verbose):
     """Something layered on top of hydra."""
-    pass
+
+    if verbose >= 1:
+        logging.basicConfig()
+        log.setLevel(logging.INFO)
+        if verbose >= 2:
+            log.setLevel(logging.DEBUG)
